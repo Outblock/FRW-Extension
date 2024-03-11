0538f8b281f39affae88291924151af92c7a3387
lmcmz <lmcmze@gmail.com>
2024-03-11 22:45:49 +1100

    Fix vertical alignment issue

--- src/ui/views/AddressImport/index.tsx
+++ src/ui/views/AddressImport/index.tsx
@@ -128,7 +128,7 @@ const AddressImport = () => {
           flexDirection: 'column',
           backgroundColor: 'background.default',
           width: '100%',
-          height: '100%',
+          height: '100vh',
           justifyContent: 'center',
           alignItems: 'center',
         }}
--- src/ui/views/WelcomePage/index.tsx
+++ src/ui/views/WelcomePage/index.tsx
@@ -22,7 +22,7 @@ const WelcomePage = () => {
           flexDirection: 'column',
           backgroundColor: 'info3.default',
           width: '100%',
-          height: '100%',
+          height: '100vh',
           justifyContent: 'center',
           alignItems: 'center',
         }}
