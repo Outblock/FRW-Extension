5fb0c779f718cf6e6e0bd1685b01af5caf4eff38
zzggo <han0303006@gmail.com>
2024-05-10 22:58:17 +1000

    feat: viewmore

--- src/ui/views/Dashboard/Components/Popup.tsx
+++ src/ui/views/Dashboard/Components/Popup.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Box, ListItemButton, Typography, Drawer, IconButton, ListItem, ListItemIcon, ListItemText, Avatar, CardMedia } from '@mui/material';
+import { Box, Button, ListItemButton, Typography, Drawer, IconButton, ListItem, ListItemIcon, ListItemText, Avatar, CardMedia } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useWallet } from 'ui/utils';
 import { useHistory } from 'react-router-dom';
 import popLock from 'ui/FRWAssets/svg/popLock.svg';
 import popAdd from 'ui/FRWAssets/svg/popAdd.svg';
 import iconCheck from 'ui/FRWAssets/svg/iconCheck.svg';
+import vmsvg from 'ui/FRWAssets/svg/viewmore.svg';
 
 
 
@@ -30,7 +31,7 @@ const Popup = (props: TransferConfirmationProps) => {
 
   const usewallet = useWallet();
   const history = useHistory();
-  // const [validated, setValidated] = useState<any>(null);
+  const [viewmore, setMore] = useState<boolean>(false);
   // console.log('props.loggedInAccounts', props.current)
 
   return (
@@ -65,55 +66,79 @@ const Popup = (props: TransferConfirmationProps) => {
           </Box>
         </Box>
         <Box component="nav">
-          {Array.isArray(props.loggedInAccounts) && props.loggedInAccounts.map((user, index) => {
-            const userWithIndex = {
-              ...user,
-              indexInLoggedInAccounts: index
-            };
-            return (
-              <ListItem disablePadding key={user.username} onClick={() => {
-                if (user.username !== props.userInfo.username) {
-                  props.switchAccount(userWithIndex);
-                }
-              }}>
-                <ListItemIcon>
-                  <Avatar
-                    component="span"
-                    src={user.avatar}
-                    sx={{ width: '32px', height: '32px' }}
-                    alt="avatar"
-                  />
-                </ListItemIcon>
-                <ListItemText>
-                  <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                    <Typography
-                      variant="body1"
-                      component="div"
-                      display="inline"
-                      color='text.primary'
-                    >
-                      {user.username}
-                    </Typography>
-                    <Typography
-                      variant="body1"
-                      component="div"
-                      display="inline"
-                      color="text.secondary"
-                      sx={{ fontSize: '12px' }}
-                    >
-                      {user.address ? user.address : user.nickname}
-                    </Typography>
-                  </Box>
-                </ListItemText>
-                {user.username === props.userInfo.username &&
-                  <CardMedia component="img" sx={{ width: '16px', height: '16px' }} image={iconCheck} />
-                }
-              </ListItem>
-            );
-          })}
+          {Array.isArray(props.loggedInAccounts) &&
+            <Box
+              sx={{
+                justifyContent: 'space-between',
+                position: 'relative',
+                alignItems: 'center',
+                flexDirection: 'column',
+                display: 'flex',
+                height: viewmore ? '246px' : '166px',
+                overflow: viewmore ? 'scroll' : 'hidden',
+                paddingBottom: '16px'
+              }}
+            >
+              {props.loggedInAccounts.map((user, index) => {
+                const userWithIndex = {
+                  ...user,
+                  indexInLoggedInAccounts: index
+                };
+                return (
+                  <ListItem disablePadding key={user.username} onClick={() => {
+                    if (user.username !== props.userInfo.username) {
+                      props.switchAccount(userWithIndex);
+                    }
+                  }}>
+                    <ListItemIcon>
+                      <Avatar
+                        component="span"
+                        src={user.avatar}
+                        sx={{ width: '32px', height: '32px' }}
+                        alt="avatar"
+                      />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                        <Typography
+                          variant="body1"
+                          component="div"
+                          display="inline"
+                          color='text.primary'
+                        >
+                          {user.username}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          component="div"
+                          display="inline"
+                          color="text.secondary"
+                          sx={{ fontSize: '12px' }}
+                        >
+                          {user.address ? user.address : user.nickname}
+                        </Typography>
+                      </Box>
+                    </ListItemText>
+                    {user.username === props.userInfo.username &&
+                      <CardMedia component="img" sx={{ width: '16px', height: '16px' }} image={iconCheck} />
+                    }
+                  </ListItem>
+                );
+              })}
+              {(!viewmore && props.loggedInAccounts.length > 3) &&
+                <Button
+                  sx={{ display: 'flex', position: 'absolute', bottom: '0px', alignItems: 'center', background: '#2C2C2C', borderRadius: '8px', color: '#8C9BAB', textTransform: 'capitalize', padding: '4px 16px' }}
+                  onClick={() => setMore(true)}>
+                  View More
+                  <CardMedia component="img" sx={{ width: '16px', height: '16px', display: 'inline', paddingLeft: '8px' }} image={vmsvg} />
+                </Button>
+              }
+            </Box>
+
+          }
         </Box>
 
-        <Box sx={{ height: '1px', width: '100%', margin: '16px 0', backgroundColor: 'rgba(255, 255, 255, 0.12)' }}></Box>
+        <Box sx={{ height: '1px', width: '100%', margin: '0 0 16px', backgroundColor: 'rgba(255, 255, 255, 0.12)' }}></Box>
 
         <Box
           sx={{
