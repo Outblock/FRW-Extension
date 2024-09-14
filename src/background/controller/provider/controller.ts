9c4ff95117499bb1de19a944e5ff7af5b985c33d
lmcmz <lmcmze@gmail.com>
2024-09-15 02:19:28 +1000

    Update controller.ts

--- src/background/controller/provider/controller.ts
+++ src/background/controller/provider/controller.ts
@@ -7,7 +7,7 @@ import {
   sessionService,
   signTextHistoryService,
   keyringService,
-  notificationService
+  notificationService,
 } from 'background/service';
 import Wallet from '../wallet';
 import BaseController from '../base';
@@ -17,14 +17,17 @@ import {
   normalize as normalizeAddress,
   recoverPersonalSignature,
 } from 'eth-sig-util';
-import { ethers } from "ethers";
-import { sha256, isHexString, ecsign, intToHex } from "ethereumjs-util";
+import { ethers } from 'ethers';
+import { sha256, isHexString, ecsign, intToHex } from 'ethereumjs-util';
 import BigNumber from 'bignumber.js';
 import RLP from 'rlp';
 import HDWallet from 'ethereum-hdwallet';
 import Web3 from 'web3';
 import { signWithKey, seed2PubKey } from '@/ui/utils/modules/passkey.js';
-import { ensureEvmAddressPrefix, isValidEthereumAddress } from '@/ui/utils/address';
+import {
+  ensureEvmAddressPrefix,
+  isValidEthereumAddress,
+} from '@/ui/utils/address';
 import { storage } from '../../webapi';
 import { error } from 'console';
 
@@ -71,45 +74,49 @@ function removeHexPrefix(hexString: string): string {
   return hexString.startsWith('0x') ? hexString.substring(2) : hexString;
 }
 function toHexString(bytes: Uint8Array): string {
-  return Array.from(bytes).map(byte => byte.toString(16).padStart(2, '0')).join('');
+  return Array.from(bytes)
+    .map((byte) => byte.toString(16).padStart(2, '0'))
+    .join('');
 }
 
-function createAndEncodeCOAOwnershipProof(keyIndices: bigint[], address: Uint8Array, capabilityPath: string, signatures: Uint8Array[]): Uint8Array {
+function createAndEncodeCOAOwnershipProof(
+  keyIndices: bigint[],
+  address: Uint8Array,
+  capabilityPath: string,
+  signatures: Uint8Array[]
+): Uint8Array {
   const proof: COAOwnershipProof = {
     keyIndices,
     address,
     capabilityPath,
-    signatures
+    signatures,
   };
   // Prepare data for RLP encoding
   const encodedData = RLP.encode([
-    proof.keyIndices.map(index => Buffer.from(index.toString(16), 'hex')), // Convert bigint to Buffer
+    proof.keyIndices.map((index) => Buffer.from(index.toString(16), 'hex')), // Convert bigint to Buffer
     proof.address,
     Buffer.from(proof.capabilityPath, 'utf8'),
-    proof.signatures
+    proof.signatures,
   ]);
 
   return encodedData; // Convert the encoded data to a hexadecimal string for easy display or transmission
 }
 
 async function signMessage(keyring, msgParams, opts = {}) {
-
-
   const web3 = new Web3();
   const textData = msgParams.data;
 
   const rightPaddedHexBuffer = (value: string, pad: number) =>
-    Buffer.from(value.padEnd(pad * 2, '0'), 'hex')
+    Buffer.from(value.padEnd(pad * 2, '0'), 'hex');
   const hashedData = web3.eth.accounts.hashMessage(textData);
 
   const USER_DOMAIN_TAG = rightPaddedHexBuffer(
     Buffer.from('FLOW-V0.0-user').toString('hex'),
     32
-  ).toString('hex')
+  ).toString('hex');
 
-
-  const prependUserDomainTag = (msg: string) => USER_DOMAIN_TAG + msg
-  const signableData = prependUserDomainTag(removeHexPrefix(hashedData))
+  const prependUserDomainTag = (msg: string) => USER_DOMAIN_TAG + msg;
+  const signableData = prependUserDomainTag(removeHexPrefix(hashedData));
 
   // Retrieve the private key from the wallet (assuming Ethereum wallet)
   const password = keyringService.password;
@@ -117,17 +124,24 @@ async function signMessage(keyring, msgParams, opts = {}) {
   const hashAlgo = await storage.get('hashAlgo');
   const signAlgo = await storage.get('signAlgo');
   // const wallet = new ethers.Wallet(privateKey);
-  const signature = await signWithKey(signableData, signAlgo, hashAlgo, privateKey);
+  const signature = await signWithKey(
+    signableData,
+    signAlgo,
+    hashAlgo,
+    privateKey
+  );
   const currentWallet = await Wallet.getMainWallet();
 
   const addressHex = currentWallet;
   const addressBuffer = Buffer.from(addressHex.slice(2), 'hex');
   const addressArray = Uint8Array.from(addressBuffer);
 
-  const encodedProof = createAndEncodeCOAOwnershipProof([BigInt(0)], addressArray, 'evm', [Uint8Array.from(Buffer.from(signature, 'hex'))]);
-
-
-
+  const encodedProof = createAndEncodeCOAOwnershipProof(
+    [BigInt(0)],
+    addressArray,
+    'evm',
+    [Uint8Array.from(Buffer.from(signature, 'hex'))]
+  );
 
   return '0x' + toHexString(encodedProof);
 }
@@ -153,9 +167,8 @@ class ProviderController extends BaseController {
         { height: 599 }
       );
 
-      return []
+      return [];
     }
-
 
     let res: string | null;
     try {
@@ -185,26 +198,24 @@ class ProviderController extends BaseController {
     return account;
   };
   ethEstimateGas = async ({ data }) => {
-
     const network = await Wallet.getNetwork();
-    const url = EVM_ENDPOINT[network]
-    const provider = new ethers.JsonRpcProvider(url)
+    const url = EVM_ENDPOINT[network];
+    const provider = new ethers.JsonRpcProvider(url);
     const gas = await provider.estimateGas({
       from: data.params[0].from,
       // Wrapped ETH address
       to: data.params[0].to,
       // `function deposit() payable`
-      data: "0xd0e30db0",
+      data: '0xd0e30db0',
       // 1 ether
-      value: data.params[0].value
+      value: data.params[0].value,
     });
     return '0x' + gas.toString(16);
   };
 
   ethSendTransaction = async (data) => {
-
     if (!data || !data.data || !data.data.params || !data.data.params.length) {
-      console.error("Invalid data structure");
+      console.error('Invalid data structure');
       return null;
     }
 
@@ -229,7 +240,6 @@ class ProviderController extends BaseController {
 
     let currentWallet;
     try {
-
       // Attempt to query the currentNetwork address
       currentWallet = await Wallet.getMainWallet();
     } catch (error) {
@@ -244,7 +254,7 @@ class ProviderController extends BaseController {
         { height: 599 }
       );
 
-      return
+      return;
     }
 
     let res: string | null;
@@ -270,13 +280,12 @@ class ProviderController extends BaseController {
     const account = res ? [res.toLowerCase()] : [];
     await sessionService.broadcastEvent('accountsChanged', account);
     await permissionService.getConnectedSite(origin);
-    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    console.log('evmAddress ', account)
+    console.log('evmAddress ', account);
     await delay(2000);
 
     return account;
-    // return ['000000000000000000000002f9e3b9cbbaa99770'];
   };
 
   walletRequestPermissions = ({ data: { params: permissions } }) => {
@@ -301,9 +310,7 @@ class ProviderController extends BaseController {
       chainId = `0x${new BigNumber(chainId).toString(16).toLowerCase()}`;
     }
 
-
     switch (chainId) {
-
       case '0x221': // 545 in decimal corresponds to testnet
         console.log('Switch to Testnet');
         if (network !== 'testnet') {
@@ -332,7 +339,10 @@ class ProviderController extends BaseController {
         return null;
       default:
         console.log(`Unsupported ChainId: ${chainId}`);
-        return new Error(`Unsupported ChainId: ${chainId}`);
+        throw ethErrors.provider.custom({
+          code: 4902,
+          message: `Unrecognized  ChainId"${chainId}".`,
+        });
     }
   };
 
@@ -361,7 +371,6 @@ class ProviderController extends BaseController {
   };
 
   ethChainId = async ({ session }) => {
-
     const network = await Wallet.getNetwork();
     if (network === 'testnet') {
       return 545;
