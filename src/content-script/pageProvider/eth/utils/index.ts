f966f59c42161bb466ec70b0c1fdf23074951676
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-11-22 15:48:16 +1100

    Changed package manager to pnpm and updated imports

--- src/content-script/pageProvider/eth/utils/index.ts
+++ src/content-script/pageProvider/eth/utils/index.ts
@@ -1,6 +1,5 @@
 import { keyBy } from 'lodash';
 import browser from 'webextension-polyfill';
-import { ledgerUSBVendorId } from '@ledgerhq/devices';
 
 import BroadcastChannelMessage from './message/broadcastChannelMessage';
 import PortMessage from './message/portMessage';
