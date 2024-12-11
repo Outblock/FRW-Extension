a7e182401d4ec72e032b5a9d1aea3597d1cfd74e
zzggo <han0303006@gmail.com>
2024-12-11 17:28:33 +1100

    fixed: updatte welcome page

--- src/ui/FRWComponent/MainPages/index.ts
+++ src/ui/FRWComponent/MainPages/index.ts
@@ -2,3 +2,4 @@ export { default as AllSet } from './AllSet';
 export { default as PickUsername } from './PickUsername';
 export { default as RepeatPhrase } from './RepeatPhrase';
 export { default as RegisterHeader } from './RegisterHeader';
+export { default as WelcomePage } from './WelcomePage';
