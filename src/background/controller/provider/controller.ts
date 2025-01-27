0834f433bef18aec5adc41ce5eae4faff201d414
han@outblock.io <han@outblock.io>
2025-01-27 21:41:18 +1100

    fixed: roll back default for data

--- src/background/controller/provider/controller.ts
+++ src/background/controller/provider/controller.ts
@@ -280,7 +280,7 @@ class ProviderController extends BaseController {
     const gas = transactionParams.gas || '0x76c0';
     const to = transactionParams.to || '';
     const value = transactionParams.value || '0x0';
-    const dataValue = transactionParams.data || '0x0';
+    const dataValue = transactionParams.data || '0x';
     // console.log('transactionParams ', transactionParams)
     let result = await Wallet.dapSendEvmTX(to, gas, value, dataValue);
     if (!result) {
