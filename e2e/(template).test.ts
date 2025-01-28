5871df25ce3861417c99639284cbbde05257027f
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-01-28 15:27:00 +1100

    Corrected test template

--- e2e/(template).test.ts
+++ e2e/(template).test.ts
@@ -1,9 +1,11 @@
 /*
+import { test, loginToExtension } from './utils/helper';
+
 // Uncomment and duplicate this test to create a new test for recording
 // Turn off the cleanup in the IDE to maintain the user account to save registering again
 test('new test', async ({ page, extensionId }) => {
   await loginToExtension({ page, extensionId });
-  page.pause();
+  await page.pause();
   //
 });
- */
+*/
