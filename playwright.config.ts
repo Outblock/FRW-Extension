5fda261dff1f132386b5eb43469e3a3452aa5fe1
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-01-31 16:51:27 +1100

    Run in headless mode on ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     video: process.env.CI ? 'on-first-retry' : 'off',
     screenshot: process.env.CI ? 'only-on-failure' : 'off',
+    headless: process.env.CI ? true : false,
   },
   // globalTimeout: 160 * 1000,
   //globalSetup: './e2e/utils/global.setup.ts',
@@ -53,6 +54,7 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
         // Chrome extension testing configuration
         browserName: 'chromium',
+        headless: process.env.CI ? true : false,
       },
       dependencies: ['setup'],
     },
