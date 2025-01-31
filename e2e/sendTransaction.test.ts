4f5b91afa4f75e6949e6e163350d4c0a29fa983b
Zhixin Zhou <zhixin.zhou@student.unsw.edu.au>
2025-01-31 12:29:37 +1100

    add note

--- e2e/sendTransaction.test.ts
+++ e2e/sendTransaction.test.ts
@@ -3,7 +3,7 @@ export const sendTokenfromCOAtoCOA = async ({ page, tokenname, receiver }) => {
   // Wait for the EVM account to be loaded
   await getCurrentAddress(page);
   await page.getByRole('tab', { name: 'coins' }).click();
-  // send FLOW token from COA to COA
+  // send Ft token from COA to COA
   await page.getByRole('button', { name: tokenname }).click();
   await page.getByRole('button', { name: 'Send' }).click();
   await page.getByPlaceholder('Search address(0x), or flow').click();
@@ -11,10 +11,8 @@ export const sendTokenfromCOAtoCOA = async ({ page, tokenname, receiver }) => {
   await page.getByPlaceholder('Amount').fill('0.000112134354657');
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByRole('button', { name: 'Send' }).click();
-
   await page.waitForURL(/.*dashboard\?activity=1/);
   const progressBar = page.getByRole('progressbar');
-
   await expect(progressBar).toBeVisible();
   await expect(page.locator('li').first().filter({ hasText: 'Pending' })).toBeVisible({
     timeout: 60_000,
@@ -34,27 +32,31 @@ test('send other-FT COA to COA', async ({ page, extensionId }) => {
   await page.getByLabel('menu').click();
   // switch to COA account
   await page.getByRole('button', { name: 'EVM' }).nth(0).click();
-
+  // Send FLOW token from COA to COA
   await sendTokenfromCOAtoCOA({
     page,
     tokenname: /^FLOW \$/i,
     receiver: process.env.TEST_RECEIVER_EVMADDR!,
   });
+  // Send stFLOW token from COA to COA
   await sendTokenfromCOAtoCOA({
     page,
     tokenname: 'Liquid Staked Flow $',
     receiver: process.env.TEST_RECEIVER_EVMADDR!,
   });
+  // Send USDC token from COA to COA
   await sendTokenfromCOAtoCOA({
     page,
     tokenname: 'Bridged USDC (Celer) $',
     receiver: process.env.TEST_RECEIVER_EVMADDR!,
   });
+  // Send BETA token from COA to COA
   await sendTokenfromCOAtoCOA({
     page,
     tokenname: 'BETA $',
     receiver: process.env.TEST_RECEIVER_EVMADDR!,
   });
+  // Send TRUMP token from COA to COA
   await sendTokenfromCOAtoCOA({
     page,
     tokenname: 'OFFICIAL TRUMP $',
